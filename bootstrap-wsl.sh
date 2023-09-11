d8c55063eea9a47925948e7803168f5cc7cc616a
LeoLuxo <leo@gamboa.eu>
2023-09-11 21:41:22 +0200

    Add boostrappers again

---
+++ bootstrap-wsl.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+echo "Running this script will overwrite ALL dotfiles on this system."
+read -p "Make sure you are on *WSL*! Continue (y/n)?" -n 1 -r
+echo
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+	export DOTFILES_REPO="$HOME/.dotfiles"
+	function dotfiles {
+		GIT_DIR=$DOTFILES_REPO GIT_WORK_TREE=$HOME $@
+	}
+	
+	cd $HOME
+	rm -rf $DOTFILES_REPO
+	
+	git clone -b wsl --single-branch --bare https://github.com/LeoLuxo/dotfiles.git $DOTFILES_REPO
+
+	dotfiles git checkout --force
+	dotfiles git config status.showUntrackedFiles no
+	
+	sudo rm "/etc/wsl.conf"
+	sudo ln "$HOME/.wsl.conf" "/etc/wsl.conf"
+else
+	[[ "$0" = "$BASH_SOURCE" ]] && exit 1 || return 1 # handle exits from shell or function but don't exit interactive shell
+fi
